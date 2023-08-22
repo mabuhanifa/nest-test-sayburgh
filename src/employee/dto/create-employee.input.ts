0aa9e22defea78bf9ffc2771b69be7eba50dd93b
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-22 18:59:18 +0600

    CreateEmployeeInput

--- src/employee/dto/create-employee.input.ts
+++ src/employee/dto/create-employee.input.ts
@@ -1,7 +1,15 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateEmployeeInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  email: string;
 }
