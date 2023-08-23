bf4658e9a45ffe39dee2f2bddffc13798fd0b3c2
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-23 18:30:13 +0600

    relations updated

--- src/employee/entities/employee.entity.ts
+++ src/employee/entities/employee.entity.ts
@@ -1,6 +1,13 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { ContactInfo } from 'src/contact-info/entities/contact-info.entity';
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Task } from 'src/task/entities/task.entity';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'employee' })
 @ObjectType({ description: 'This is Employee Object Type' })
@@ -17,4 +24,9 @@ export class Employee {
   @Column()
   @Field(() => String)
   email: string;
+
+  @OneToMany(() => Task, (task) => task.employee)
+  @Column()
+  @Field(() => String)
+  tasks: string;
 }
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,7 +1,21 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Employee } from 'src/employee/entities/employee.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@Entity({ name: 'task' })
+@ObjectType({ description: 'This is Task Object Type' })
 export class Task {
+  @PrimaryGeneratedColumn('increment')
   @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  id: number;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @ManyToOne(() => Employee, (employee) => employee.tasks, {
+    onDelete: 'SET NULL',
+  })
+  @Field(() => Employee)
+  employee: Employee;
 }
