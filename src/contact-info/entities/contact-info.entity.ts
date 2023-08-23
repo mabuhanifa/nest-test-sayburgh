9fa75ce8f2d9755270627b94ac8ba382078ec159
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-23 17:19:11 +0600

    ContactInfo entity

--- src/contact-info/entities/contact-info.entity.ts
+++ src/contact-info/entities/contact-info.entity.ts
@@ -1,7 +1,22 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Employee } from 'src/employee/entities/employee.entity';
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 export class ContactInfo {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('increment')
+  @Field(() => Int)
+  id: number;
+
+  @Column()
+  @Field(() => String)
+  phone: string;
+
+  @Column()
+  @Field(() => Int)
+  employeeId: number;
+
+  @OneToOne(() => Employee, (employee) => employee.id)
+  @JoinColumn()
+  employee: Employee;
 }
