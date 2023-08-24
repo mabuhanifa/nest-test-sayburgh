d1e6696d4ba80f017a9910ef837d0ff58ab030e2
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-24 14:35:22 +0600

    Department entity

--- src/department/entities/department.entity.ts
+++ src/department/entities/department.entity.ts
@@ -1,7 +1,26 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Employee } from 'src/employee/entities/employee.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
-@ObjectType()
+@Entity({ name: 'department' })
+@ObjectType({ description: 'This is department Object Type' })
 export class Department {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('increment')
+  @Field(() => Int)
+  id: number;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @OneToMany(() => Employee, (employee) => employee.id)
+  @JoinColumn()
+  @Field(() => [String])
+  employees: Employee[];
 }
