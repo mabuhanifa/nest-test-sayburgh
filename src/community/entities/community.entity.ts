fbd6714f802840f46f220f071e7634fea4246499
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-27 18:33:56 +0600

    CreateUserInput updated

--- src/community/entities/community.entity.ts
+++ src/community/entities/community.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Column, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 export class Community {
@@ -10,4 +11,7 @@ export class Community {
   @Column()
   @Field(() => String)
   name: string;
+
+  @ManyToMany(() => User, (user) => user.communities)
+  user: User[];
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,21 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { PrimaryGeneratedColumn } from 'typeorm';
+import { Community } from 'src/community/entities/community.entity';
+import { Column, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 export class User {
   @PrimaryGeneratedColumn('increment')
   @Field(() => Int)
   id: number;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @ManyToMany(() => Community, (community) => community.user, {
+    cascade: true,
+  })
+  @JoinTable()
+  @Field(() => [Community])
+  communities: Community[];
 }
