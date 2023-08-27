bd71e30a99a9f80e7d37a3d56c89e49433052862
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-27 13:15:09 +0600

    UpdateCategoryInput

--- src/category/dto/update-category.input.ts
+++ src/category/dto/update-category.input.ts
@@ -1,8 +1,16 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { CreateCategoryInput } from './create-category.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateCategoryInput extends PartialType(CreateCategoryInput) {
+  @IsNotEmpty()
+  @IsString()
   @Field(() => Int)
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  name: string;
 }
