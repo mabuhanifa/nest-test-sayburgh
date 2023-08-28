644fa56e30a399f560c27d019bebe51145a8f6a9
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-28 16:33:29 +0600

    UpdateUserInput

--- src/user/dto/update-user.input.ts
+++ src/user/dto/update-user.input.ts
@@ -1,8 +1,21 @@
+import { Field, InputType, Int, PartialType } from '@nestjs/graphql';
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
+  @IsNotEmpty()
+  @IsNumber()
   @Field(() => Int)
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Field(() => [Int], { nullable: true })
+  communities: number[];
 }
