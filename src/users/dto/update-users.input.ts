7c3bc2c9803321ce10b39729ddfbd181f3f4de76
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-29 15:56:05 +0600

    users removed

--- src/users/dto/update-users.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserInput } from './create-users.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
-}
