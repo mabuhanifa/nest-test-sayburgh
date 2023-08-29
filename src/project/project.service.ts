a93a0e9ed7249a7475ee268f9601955d5aee6a25
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-29 17:17:46 +0600

    findAllByID

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -4,17 +4,20 @@ import { UpdateProjectInput } from './dto/update-project.input';
 import { Project } from './entities/project.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EmployeeService } from 'src/employee/employee.service';
 
 @Injectable()
 export class ProjectService {
   constructor(
     @InjectRepository(Project)
     private readonly projectRepository: Repository<Project>,
+    private readonly employeeService: EmployeeService,
   ) {}
 
   create(createProjectInput: CreateProjectInput) {
     const project = new Project();
     project.name = createProjectInput.name;
+    const employees = this.employeeService.findAll();
     return this.projectRepository.save(project);
   }
 
