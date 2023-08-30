20999024c31e0fabc14eca3fba8460e548fc8a97
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-30 16:46:21 +0600

    login without jwt working

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,14 +1,17 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 import { LoginResponse } from './dto/login-response';
 import { LoginUserInput } from './dto/login-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from './guards/local-auth.guards';
 
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Mutation(() => LoginResponse)
+  // @UseGuards(GqlAuthGuard)
   login(@Args('loginUserInput') loginUserInput: LoginUserInput) {
     return this.authService.login(loginUserInput);
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,8 @@ import { UpdateAuthInput } from './dto/update-auth.input';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import { User } from 'src/user/entities/user.entity';
 import { LoginUserInput } from './dto/login-user.input';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -16,8 +16,9 @@ export class AuthService {
 
   async validateUser(name: string, password: string) {
     const user = await this.userService.findOneByName(name);
+    console.log(user);
 
-    if (user && (await bcrypt.compare(password, user.password))) {
+    if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
@@ -27,23 +28,20 @@ export class AuthService {
 
   async login(loginUserInput: LoginUserInput) {
     const user = await this.userService.findOneByName(loginUserInput.name);
+    console.log(user);
 
-    if (!user) {
-      return null;
-    }
+    // const payload = {
+    //   username: user.name,
 
-    const payload = {
-      username: user.name,
-
-      sub: {
-        name: user.name,
-      },
-    };
+    //   sub: {
+    //     name: user.name,
+    //   },
+    // };
 
     return {
-      user: user.name,
+      user: user,
 
-      access_token: this.jwtService.sign(payload),
+      access_token: 'this.jwtService.sign(payload)',
     };
   }
 }
