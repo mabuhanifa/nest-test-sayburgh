fa303328b2914d20cd6caad4d9550389a258a52d
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-03 12:56:31 +0600

    JwtModule imported in every modules

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -4,9 +4,16 @@ import { ProjectResolver } from './project.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import EmployeeModule from 'src/employee/employee.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), EmployeeModule],
+  imports: [
+    TypeOrmModule.forFeature([Project]),
+    EmployeeModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   providers: [ProjectResolver, ProjectService],
   exports: [ProjectService],
 })
